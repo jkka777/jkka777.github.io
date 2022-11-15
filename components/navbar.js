ef98f9f129508aaf58176e654d0aa3f705dc2a14
jkka777 <abhiram.kishore@gmail.com>
2022-11-15 12:10:39 +0530

    added util images and little modifiactions

--- components/navbar.js
+++ components/navbar.js
@@ -17,13 +17,13 @@ let navbar = () => {
 let footer = () => {
     return `
     <div><img
-                src="https://imgs.search.brave.com/xViDn4jLsf4MX2JcyTAMcrpHnnBpvbts2cSVffn6emE/rs:fit:1024:1024:1/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy9j/L2NhL0xpbmtlZElu/X2xvZ29faW5pdGlh/bHMucG5n"
+                src="/util/linkedin.webp"
                 alt="linkedn logo"></div>
         <div><img
-                src="https://imgs.search.brave.com/pTRxp49h_VbVkhHasbi3ThNs_i8jOhBM0NfTGZ68Sro/rs:fit:280:280:1/g:ce/aHR0cHM6Ly9sb2dv/ZGl4LmNvbS9sb2dv/Lzk1Mjg5MC5wbmc"
+                src="/util/github.webp"
                 alt="github-logo"></div>
         <div>
-            <img src="https://imgs.search.brave.com/INSRq5CU0U0vTJcSJrAvY0eLvA-pj8T5MXF96hliv7M/rs:fit:1200:1104:1/g:ce/aHR0cDovL3RvdXMt/bG9nb3MuY29tL3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE3LzA1/L1R3aXR0ZXItbG9n/by5wbmc"
+            <img src="/util/twitter.webp"
                 alt="twitter-logo">
         </div>
     `
--- scripts/index.js
+++ scripts/index.js
@@ -152,38 +152,38 @@ let skillsPage = () => {
 
     let skill1 = createElem('div');
     let skill1_img = createElem('img');
-    skill1_img.src = 'https://imgs.search.brave.com/NzopLX68q-PUcI2j3DhmRmie1ffbO6NbFkdgm-OQY0o/rs:fit:900:925:1/g:ce/aHR0cHM6Ly93d3cu/Y2xpcGFydGtleS5j/b20vbXBuZ3MvbS8y/MTAtMjEwNDcwNV9o/dG1sLWxvZ28tcG5n/LXRyYW5zcGFyZW50/LWJhY2tncm91bmQu/cG5n';
+    skill1_img.src = 'util/html.webp';
     skill1_img.setAttribute('alt', 'html icon');
     skill1.append(skill1_img);
 
     let skill2 = createElem('div');
     let skill2_img = createElem('img');
-    skill2_img.src = 'https://imgs.search.brave.com/RDCE6e2-TlvotB5ryZDLWVUGPao0AV0Yubc3ettA-LI/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9jZG4u/ZnJlZWJpZXN1cHBs/eS5jb20vbG9nb3Mv/bGFyZ2UvMngvY3Nz/My1sb2dvLXBuZy10/cmFuc3BhcmVudC5w/bmc';
+    skill2_img.src = 'util/css.webp';
     skill2_img.setAttribute('alt', 'css icon');
     skill2.append(skill2_img);
 
     let skill3 = createElem('div');
     let skill3_img = createElem('img');
-    skill3_img.src = 'https://imgs.search.brave.com/H4pIdYMAme1di27SUCv61oTSX_jZ0eSDuzomjq2lsRw/rs:fit:1052:1052:1/g:ce/aHR0cDovLzMuYnAu/YmxvZ3Nwb3QuY29t/Ly1QVHR5M0NmVEdu/QS9UcFpPRWpUUV9X/SS9BQUFBQUFBQUFl/by9LZUt0X0Q1WDJ4/by9zMTYwMC9qcy5q/cGc';
+    skill3_img.src = 'util/javascript.webp';
     skill3_img.setAttribute('alt', 'js icon');
     skill3.append(skill3_img);
 
     let skill4 = createElem('div');
     let skill4_img = createElem('img');
-    skill4_img.src = 'https://imgs.search.brave.com/DUR9TwXHA5oH-kXMndhMbAiZ2p7wwKNbXleC3G0ioqI/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/cGlrcG5nLmNvbS9w/bmdsL2IvMTQ2LTE0/NjkxNDZfamF2YS1s/b2dvLXRyYW5zcGFy/ZW50LXBuZy5wbmc';
+    skill4_img.src = 'util/java.webp';
     skill4_img.setAttribute('alt', 'java icon');
     skill4.append(skill4_img);
 
     let skill5 = createElem('div');
     let skill5_img = createElem('img');
-    skill5_img.src = 'https://imgs.search.brave.com/axrBqRkpyTCUbE1Pk7tRUX8nA9ExIraXw7AWEA2BLKA/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9jZG4u/ZnJlZWJpZXN1cHBs/eS5jb20vbG9nb3Mv/bGFyZ2UvMngvc3By/aW5nLTMtbG9nby1w/bmctdHJhbnNwYXJl/bnQucG5n';
-    skill5_img.setAttribute('alt', 'java icon');
+    skill5_img.src = 'util/spring.webp';
+    skill5_img.setAttribute('alt', 'spring icon');
     skill5.append(skill5_img);
 
     let skill6 = createElem('div');
     let skill6_img = createElem('img');
-    skill6_img.src = 'https://imgs.search.brave.com/J05hwr08spi9HzppBXefubcbewtnrMUwIV1BH-rzquk/rs:fit:1200:1200:1/g:ce/aHR0cDovL3BuZ2lt/Zy5jb20vdXBsb2Fk/cy9naXRodWIvZ2l0/aHViX1BORzQwLnBu/Zw';
-    skill6_img.setAttribute('alt', 'githun icon');
+    skill6_img.src = 'util/github_png.webp';
+    skill6_img.setAttribute('alt', 'github icon');
     skill6.append(skill6_img);
 
     tech_skills.append(skill1, skill2, skill3, skill4, skill5, skill6);
@@ -287,7 +287,7 @@ let projectsPage = () => {
     p1_link.style.cssText += 'text-decoration:none';
 
     let repo_link1 = createElem('img');
-    repo_link1.setAttribute('src', 'https://imgs.search.brave.com/pTRxp49h_VbVkhHasbi3ThNs_i8jOhBM0NfTGZ68Sro/rs:fit:280:280:1/g:ce/aHR0cHM6Ly9sb2dv/ZGl4LmNvbS9sb2dv/Lzk1Mjg5MC5wbmc');
+    repo_link1.setAttribute('src', 'util/github_png.webp');
     repo_link1.addEventListener('click', () => window.open('https://github.com/jkka777/bewakoof-clone'));
     repo_link1.style.cssText += 'height:26px;width:26px;cursor:pointer';
 
@@ -317,15 +317,15 @@ let projectsPage = () => {
     let lang_div = createElem('div');
     lang_div.setAttribute('id', 'lang-div-1');
     let html_logo = createElem('img');
-    html_logo.src = 'https://imgs.search.brave.com/NzopLX68q-PUcI2j3DhmRmie1ffbO6NbFkdgm-OQY0o/rs:fit:900:925:1/g:ce/aHR0cHM6Ly93d3cu/Y2xpcGFydGtleS5j/b20vbXBuZ3MvbS8y/MTAtMjEwNDcwNV9o/dG1sLWxvZ28tcG5n/LXRyYW5zcGFyZW50/LWJhY2tncm91bmQu/cG5n';
+    html_logo.src = 'util/html.webp';
     // html_logo.style.height = '80px';
     // html_logo.style.width = '80px';
 
     let css_logo = createElem('img');
-    css_logo.src = 'https://imgs.search.brave.com/4cd39QU53u_hoNtEFcemVx8EhgXvYU66pGB-wSVsrhA/rs:fit:1181:1200:1/g:ce/aHR0cDovL2Jsb2cu/YnJha29uaWVja2ku/cGwvd3AtY29udGVu/dC91cGxvYWRzLzIw/MTUvMDIvY3NzLWxv/Z28ucG5n';
+    css_logo.src = 'util/css.webp';
 
     let js_logo = createElem('img');
-    js_logo.src = 'https://imgs.search.brave.com/3K6TMZFm5JnfHcMIftWgrgHnWR02dd1Es9p4DtGAYIc/rs:fit:1024:768:1/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDUxOTEz/OTkuanBn';
+    js_logo.src = 'util/javascript.webp';
 
     lang_div.append(html_logo, css_logo, js_logo);
 
@@ -336,10 +336,10 @@ let projectsPage = () => {
     let tool_div = createElem('div');
     tool_div.setAttribute('id', 'tool-div-1');
     let vs_code_logo = createElem('img');
-    vs_code_logo.src = 'https://imgs.search.brave.com/6047vQ4hJAuOrjiBbBA4HEc4OyJ0-hXbJYvc-Jk29us/rs:fit:777:774:1/g:ce/aHR0cHM6Ly9tb2Jp/bGVtYW5jZXJibG9n/LmJsb2IuY29yZS53/aW5kb3dzLm5ldC9i/bG9nLzIwMjAvMDgv/dnMtY29kZS1sb2dv/LXRyYW5zcC5wbmc';
+    vs_code_logo.src = 'util/vscode.webp';
 
     let git_logo = createElem('img');
-    git_logo.src = 'https://imgs.search.brave.com/ikcrhKQN5Ni60H22fPzkPeFZsD0oUqDTRZdQputksE8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9wbmdp/bWcuY29tL3VwbG9h/ZHMvZ2l0aHViL2dp/dGh1Yl9QTkc0MC5w/bmc';
+    git_logo.src = 'util/github_png.webp';
 
     tool_div.append(vs_code_logo, git_logo);
 
@@ -371,7 +371,7 @@ let projectsPage = () => {
     p2_link.style.cssText += 'text-decoration:none';
 
     let repo_link2 = createElem('img');
-    repo_link2.setAttribute('src', 'https://imgs.search.brave.com/pTRxp49h_VbVkhHasbi3ThNs_i8jOhBM0NfTGZ68Sro/rs:fit:280:280:1/g:ce/aHR0cHM6Ly9sb2dv/ZGl4LmNvbS9sb2dv/Lzk1Mjg5MC5wbmc');
+    repo_link2.setAttribute('src', 'util/github_png.webp');
     repo_link2.addEventListener('click', () => window.open('https://github.com/jkka777/cronometer-clone'));
     repo_link2.style.cssText += 'height:26px;width:26px;cursor:pointer';
 
@@ -403,13 +403,13 @@ let projectsPage = () => {
     lang_div_2.setAttribute('id', 'lang-div-1');
 
     let html_logo_2 = createElem('img');
-    html_logo_2.src = 'https://imgs.search.brave.com/NzopLX68q-PUcI2j3DhmRmie1ffbO6NbFkdgm-OQY0o/rs:fit:900:925:1/g:ce/aHR0cHM6Ly93d3cu/Y2xpcGFydGtleS5j/b20vbXBuZ3MvbS8y/MTAtMjEwNDcwNV9o/dG1sLWxvZ28tcG5n/LXRyYW5zcGFyZW50/LWJhY2tncm91bmQu/cG5n';
+    html_logo_2.src = 'util/html.webp';
 
     let css_logo_2 = createElem('img');
-    css_logo_2.src = 'https://imgs.search.brave.com/4cd39QU53u_hoNtEFcemVx8EhgXvYU66pGB-wSVsrhA/rs:fit:1181:1200:1/g:ce/aHR0cDovL2Jsb2cu/YnJha29uaWVja2ku/cGwvd3AtY29udGVu/dC91cGxvYWRzLzIw/MTUvMDIvY3NzLWxv/Z28ucG5n';
+    css_logo_2.src = 'util/css.webp';
 
     let js_logo_2 = createElem('img');
-    js_logo_2.src = 'https://imgs.search.brave.com/3K6TMZFm5JnfHcMIftWgrgHnWR02dd1Es9p4DtGAYIc/rs:fit:1024:768:1/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDUxOTEz/OTkuanBn';
+    js_logo_2.src = 'util/javascript.webp';
 
     lang_div_2.append(html_logo_2, css_logo_2, js_logo_2);
 
@@ -420,10 +420,10 @@ let projectsPage = () => {
     let tool_div_2 = createElem('div');
     tool_div_2.setAttribute('id', 'tool-div-1');
     let vs_code_logo_2 = createElem('img');
-    vs_code_logo_2.src = 'https://imgs.search.brave.com/6047vQ4hJAuOrjiBbBA4HEc4OyJ0-hXbJYvc-Jk29us/rs:fit:777:774:1/g:ce/aHR0cHM6Ly9tb2Jp/bGVtYW5jZXJibG9n/LmJsb2IuY29yZS53/aW5kb3dzLm5ldC9i/bG9nLzIwMjAvMDgv/dnMtY29kZS1sb2dv/LXRyYW5zcC5wbmc';
+    vs_code_logo_2.src = 'util/vscode.webp';
 
     let git_logo_2 = createElem('img');
-    git_logo_2.src = 'https://imgs.search.brave.com/ikcrhKQN5Ni60H22fPzkPeFZsD0oUqDTRZdQputksE8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9wbmdp/bWcuY29tL3VwbG9h/ZHMvZ2l0aHViL2dp/dGh1Yl9QTkc0MC5w/bmc';
+    git_logo_2.src = 'util/github_png.webp';
 
     tool_div_2.append(vs_code_logo_2, git_logo_2);
 
@@ -455,7 +455,7 @@ let projectsPage = () => {
     p3_link.style.cssText += 'text-decoration:none';
 
     let repo_link3 = createElem('img');
-    repo_link3.setAttribute('src', 'https://imgs.search.brave.com/pTRxp49h_VbVkhHasbi3ThNs_i8jOhBM0NfTGZ68Sro/rs:fit:280:280:1/g:ce/aHR0cHM6Ly9sb2dv/ZGl4LmNvbS9sb2dv/Lzk1Mjg5MC5wbmc');
+    repo_link3.setAttribute('src', 'util/github_png.webp');
     repo_link3.addEventListener('click', () => window.open('https://github.com/jkka777/clockify-clone'));
     repo_link3.style.cssText += 'height:26px;width:26px;cursor:pointer';
 
@@ -487,13 +487,13 @@ let projectsPage = () => {
     lang_div_3.setAttribute('id', 'lang-div-1');
 
     let html_logo_3 = createElem('img');
-    html_logo_3.src = 'https://imgs.search.brave.com/NzopLX68q-PUcI2j3DhmRmie1ffbO6NbFkdgm-OQY0o/rs:fit:900:925:1/g:ce/aHR0cHM6Ly93d3cu/Y2xpcGFydGtleS5j/b20vbXBuZ3MvbS8y/MTAtMjEwNDcwNV9o/dG1sLWxvZ28tcG5n/LXRyYW5zcGFyZW50/LWJhY2tncm91bmQu/cG5n';
+    html_logo_3.src = 'util/html.webp';
 
     let css_logo_3 = createElem('img');
-    css_logo_3.src = 'https://imgs.search.brave.com/4cd39QU53u_hoNtEFcemVx8EhgXvYU66pGB-wSVsrhA/rs:fit:1181:1200:1/g:ce/aHR0cDovL2Jsb2cu/YnJha29uaWVja2ku/cGwvd3AtY29udGVu/dC91cGxvYWRzLzIw/MTUvMDIvY3NzLWxv/Z28ucG5n';
+    css_logo_3.src = 'util/css.webp';
 
     let js_logo_3 = createElem('img');
-    js_logo_3.src = 'https://imgs.search.brave.com/3K6TMZFm5JnfHcMIftWgrgHnWR02dd1Es9p4DtGAYIc/rs:fit:1024:768:1/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDUxOTEz/OTkuanBn';
+    js_logo_3.src = 'util/javascript.webp';
 
     lang_div_3.append(html_logo_3, css_logo_3, js_logo_3);
 
@@ -504,16 +504,16 @@ let projectsPage = () => {
     let tool_div_3 = createElem('div');
     tool_div_3.setAttribute('id', 'tool-div-1');
     let vs_code_logo_3 = createElem('img');
-    vs_code_logo_3.src = 'https://imgs.search.brave.com/6047vQ4hJAuOrjiBbBA4HEc4OyJ0-hXbJYvc-Jk29us/rs:fit:777:774:1/g:ce/aHR0cHM6Ly9tb2Jp/bGVtYW5jZXJibG9n/LmJsb2IuY29yZS53/aW5kb3dzLm5ldC9i/bG9nLzIwMjAvMDgv/dnMtY29kZS1sb2dv/LXRyYW5zcC5wbmc';
+    vs_code_logo_3.src = 'util/vscode.webp';
 
     let git_logo_3 = createElem('img');
-    git_logo_3.src = 'https://imgs.search.brave.com/ikcrhKQN5Ni60H22fPzkPeFZsD0oUqDTRZdQputksE8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9wbmdp/bWcuY29tL3VwbG9h/ZHMvZ2l0aHViL2dp/dGh1Yl9QTkc0MC5w/bmc';
+    git_logo_3.src = 'util/github_png.webp';
 
     let npm_logo_3 = createElem('img');
-    npm_logo_3.src = 'https://imgs.search.brave.com/VNLK2rrw2Q-dn3-t_VSXLCccRvAAnQR-T5lo1JB5wDI/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly90cmFi/cHVrY2lwLmdhbGxl/cnljZG4udnNhc3Nl/dHMuaW8vZXh0ZW5z/aW9ucy90cmFicHVr/Y2lwL3ZzY29kZS1u/cG0tc2NyaXB0cy8w/LjIuMS8xNTI2MjU1/NDY4MTIzL01pY3Jv/c29mdC5WaXN1YWxT/dHVkaW8uU2Vydmlj/ZXMuSWNvbnMuRGVm/YXVsdA';
+    npm_logo_3.src = 'util/npm.webp';
 
     let heroku_logo_3 = createElem('img');
-    heroku_logo_3.src = 'https://imgs.search.brave.com/watvs0-D6VRBmieALGXCEDsFVjnMIP_mw4nKppk2L4Y/rs:fit:300:300:1/g:ce/aHR0cHM6Ly93ZWJi/eWdyYW0uY29tL3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE3LzEw/L2hvc3RpbmdfaGVy/b2t1LnBuZw';
+    heroku_logo_3.src = 'util/heroku.webp';
 
     tool_div_3.append(vs_code_logo_3, git_logo_3, npm_logo_3, heroku_logo_3);
 
@@ -580,7 +580,7 @@ let contactPage = () => {
     mail_div.setAttribute('id', 'mail');
 
     let mail_img = createElem('img');
-    mail_img.src = 'https://imgs.search.brave.com/3x52lJTuzSqvhNX62-kBlsNirNd2DyEJo6zM6L0a6D8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb2dv/bG9vay5uZXQvd3At/Y29udGVudC91cGxv/YWRzLzIwMjEvMDYv/R21haWwtTG9nby5w/bmc';
+    mail_img.src = 'util/gmail.webp';
 
     let mail_p = createElem('p');
     let mail_link = createElem('a');
@@ -594,7 +594,7 @@ let contactPage = () => {
     link_div.setAttribute('id', 'link');
 
     let link_img = createElem('img');
-    link_img.src = 'https://imgs.search.brave.com/YKkPwjTZAmihPFmFtO63AP9HDmfSG4xOYmLh0agmFj8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9teWNs/b3VkZG9vci5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMTkv/MTEvTGlua2VkaW4t/bG9nby5wbmc';
+    link_img.src = 'util/linkedin.webp';
 
     let link_link = createElem('a');
     link_link.textContent = 'LinkedIn';
@@ -607,7 +607,7 @@ let contactPage = () => {
     git_div.setAttribute('id', 'git');
 
     let git_img = createElem('img');
-    git_img.src = 'https://imgs.search.brave.com/J05hwr08spi9HzppBXefubcbewtnrMUwIV1BH-rzquk/rs:fit:1200:1200:1/g:ce/aHR0cDovL3BuZ2lt/Zy5jb20vdXBsb2Fk/cy9naXRodWIvZ2l0/aHViX1BORzQwLnBu/Zw';
+    git_img.src = 'util/github_png.webp';
 
     let git_link = createElem('a');
     git_link.textContent = 'GitHub';
@@ -620,7 +620,7 @@ let contactPage = () => {
     phn_div.setAttribute('id', 'phone');
 
     let phn_img = createElem('img');;
-    phn_img.src = 'https://imgs.search.brave.com/3SiNuDB6B8Lt0iRX9f9-oc2b2KZ3xgfnnzIa9p6WtqE/rs:fit:512:512:1/g:ce/aHR0cDovL3d3dy5j/bGlwYXJ0YmVzdC5j/b20vY2xpcGFydHMv/bmNFL0I3WC9uY0VC/N1hnamkucG5n';
+    phn_img.src = 'util/phone.webp';
 
     let phn_p = createElem('p');
     let phn_link = createElem('a');
