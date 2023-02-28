68c203f7b69543f51ebfdae923f974182a3a3cd6
jkka777 <abhiram.kishore@gmail.com>
2023-02-28 15:16:25 +0530

    added mobile navigation

--- scripts/index.js
+++ scripts/index.js
@@ -20,3 +20,23 @@ catchElement('.nav-resume-button').addEventListener('click', () => {
 catchElement('.resume-button').addEventListener('click', () => {
     window.open("https://drive.google.com/file/d/1G_7xHAzAIzyTWsdCWVrVXyJtqMYzrhYX/view?usp=sharing");
 });
+
+
+/* navbar menu */
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
